4dd7b2c2dfa4ab691db2fd9efdd13278c8594fca
Sergey Kazantsev <serzh.kazantseff@gmail.com>
2022-11-30 11:25:17 +0500

    Add statics handler

---
+++ kazantsev/stat_handler_test.py
@@ -0,0 +1,5 @@
+from stat_handler import execute_program
+
+# Edit the filename before testing
+execute_program("vacancies_medium.csv", "Премиум-вакансия: Нет", "", "", "",
+                "Название, Навыки, Опыт работы, Премиум-вакансия, Компания, Оклад, Название региона, Дата публикации вакансии")
