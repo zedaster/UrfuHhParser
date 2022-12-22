3f9e79b0b6a3797127dfc636823150c7b962e85a
Sergey Kazantsev <serzh.kazantseff@gmail.com>
2022-12-22 17:58:18 +0500

    Optimize pandas code in salary_handler.py

--- kazantsev/salary_handler.py
+++ kazantsev/salary_handler.py
@@ -1,39 +1,25 @@
 from pathlib import Path
 
-import numpy as np
 import pandas as pd
 
 from kazantsev.currency_rates import CurrencyRates
-from kazantsev.datetime_parser import parse_datetime
 
 
-def _add_salary_column(row, rates):
+def _salary_to_rub(row, rates):
     """
-    Получает ячейку salary для определнной строки в таблице
+    Переводит зарплату из заданной валюты в рубли
     :param row: Строка в таблица (pd.Series)
     :param rates: CurrencyRates
     :return: Значение ячейки
     """
-    amount = None
-    if pd.isnull(row['salary_from']) and pd.isnull(row['salary_to']):
-        return row
-    if pd.isnull(row['salary_from']):
-        amount = row['salary_to']
-    elif pd.isnull(row['salary_to']):
-        amount = row['salary_from']
-    else:
-        amount = (row['salary_from'] + row['salary_to']) / 2
-
-    if row['salary_currency'] != 'RUR':
-        date = parse_datetime(row['published_at'])
+    if row['salary_currency'] != 'RUR' and not pd.isnull(row['salary']):
         try:
-            rate = rates.get_rate(row['salary_currency'], date)
+            rate = rates.get_rate(row['salary_currency'], row['published_at'])
         except ValueError:
             return row
         if rate is None:
             return row
-        amount *= rate
-    row['salary'] = round(amount, 0)
+        row['salary'] = round(row['salary'] * rate, 0)
     return row
 
 
@@ -45,8 +31,11 @@ def handle_salaries(in_path: Path, out_path: Path, rates: CurrencyRates, limit=N
     :param rates: Курсы валют
     :return: None
     """
-    df = pd.read_csv(in_path, nrows=limit)
-    df.insert(1, column='salary', value=[np.nan for _ in range(df.shape[0])])
-    df = df.apply(lambda x: _add_salary_column(x, rates), axis=1)
-    df = df.drop(['salary_from', 'salary_to', 'salary_currency'], axis=1)
-    df.to_csv(out_path, index=False)
+
+    pd \
+        .read_csv(in_path, nrows=limit) \
+        .assign(published_at=lambda x: pd.to_datetime(x.published_at)) \
+        .assign(salary=lambda x: x[['salary_from', 'salary_to']].mean(axis=1)) \
+        .apply(lambda x: _salary_to_rub(x, rates), axis=1) \
+        .loc[:, ['name', 'salary', 'area_name', 'published_at']] \
+        .to_csv(out_path, index=False)
